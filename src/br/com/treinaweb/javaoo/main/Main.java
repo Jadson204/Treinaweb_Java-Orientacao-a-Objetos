166bf37d71eb1f0f92de1230be52f3e52957594b
Jadson Lima <jadsonhenrique2004@gmail.com>
2024-02-26 09:35:04 -0300

    Visibilidade private, métodos acessores e o padrão POJO

--- src/br/com/treinaweb/javaoo/main/Main.java
+++ src/br/com/treinaweb/javaoo/main/Main.java
@@ -5,13 +5,15 @@ import br.com.treinaweb.javaoo.classes.Veiculo;
 public class Main {
     public static void main(String[] args) {
         Veiculo corsa = new Veiculo();
-        corsa.marca = "GM";
-        corsa.nome = "Corsa";
+        corsa.setMarca("GM");
+        corsa.setNome("Corsa");
         corsa.ligar();
+        System.out.println(corsa.getNome());
 
         Veiculo celta = new Veiculo();
-        celta.marca = "GM";
-        celta.nome = "Celta";
+        celta.setMarca("GM");
+        celta.setNome("Celta");
         celta.ligar();
+        System.out.println(celta.getNome());
     }
 }
