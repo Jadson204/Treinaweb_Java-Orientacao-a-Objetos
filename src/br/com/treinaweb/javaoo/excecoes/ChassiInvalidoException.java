970262e3358035925eb24f55214e9c1f95dd8f68
Jadson Lima <jadsonhenrique2004@gmail.com>
2024-02-27 12:21:49 -0300

    Criando construtores personalizados

--- src/br/com/treinaweb/javaoo/excecoes/ChassiInvalidoException.java
+++ src/br/com/treinaweb/javaoo/excecoes/ChassiInvalidoException.java
@@ -2,4 +2,7 @@ package br.com.treinaweb.javaoo.excecoes;
 
 public class ChassiInvalidoException extends Exception {
 
+    public ChassiInvalidoException(String chassi) {
+        super(String.format("Este chassi é inválido [%s]", chassi));
+    }
 }
